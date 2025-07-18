63e2d098c00f8858125437d0ed5e4121fed18cee
BossAkew <farisrizky23@gmail.com>
2025-07-18 10:14:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-1. import streamlit as st
-2. import joblib
-3. import numpy as np
-4. 
-5. # Memuat model regresi linear yang sudah disimpan
-6. lin_reg_loaded = joblib.load('lin_reg_model.joblib')
-7. 
-8. # Judul aplikasi
-9. st.title("Prediksi Gaji Berdasarkan Lama Bekerja")
-10. 
-11. # Input tahun pengalaman kerja
-12. years_experience = st.number_input(
-13.   "Masukkan jumlah tahun bekerja:", 
+import streamlit as st
+import joblib
+import numpy as np
+ 
+# Memuat model regresi linear yang sudah disimpan
+lin_reg_loaded = joblib.load('lin_reg_model.joblib')
+ 
+# Judul aplikasi
+st.title("Prediksi Gaji Berdasarkan Lama Bekerja")
+ 
+# Input tahun pengalaman kerja
+years_experience = st.number_input(
+  "Masukkan jumlah tahun bekerja:", 
     min_value=0.0, 
     step=0.1
 )
